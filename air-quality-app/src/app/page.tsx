daa81fcc86a9b6275eed443cc897fa8b34c74010
Andrei-Laurentiu Ciurtin <149907852+CiurtinAndrei@users.noreply.github.com>
2024-12-25 19:51:46 +0200

    initial commit

---
+++ air-quality-app/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+export default function Home() {
+
+  return(
+    <>
+    <header>
+    <h1>Hello!</h1>
+
+    <Link href="/test">Test</Link>
+
+    </header>
+
+
+    </>
+);
+
+}
---
+++ air-quality-app/src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function Test() {
+
+
+    return (
+        <>
+
+        <h1> This is a test!</h1>
+        <Link href="/">Return</Link>
+
+        </>
+
+
+    );
+
+}
