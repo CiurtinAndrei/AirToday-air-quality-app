5db5e84a6f7797e8f405c394d1b12783bfb8c77d
CiurtinAndrei <149907852+CiurtinAndrei@users.noreply.github.com>
2025-01-03 18:17:42 +0200

    UI update

--- src/app/calitate-aer/page.tsx
+++ src/app/calitate-aer/page.tsx
@@ -7,25 +7,57 @@ import axios from 'axios'
 
 export default function CalitateAer() {
     
-    let queryCity:string|null
-    
-    type AirQualityData = {
-        data: {
-          city: {
-            name: string;
-          };
-          aqi: number;
-          iaqi: {
-            [key: string]: { v: number };
-          };
+  type AirQualityData = {
+    status: string;
+    data: {
+      aqi: number;
+      idx: number;
+      attributions: {
+        url: string;
+        name: string;
+        logo?: string; // Optional property
+      }[];
+      city: {
+        geo: [number, number];
+        name: string;
+        url: string;
+        location?: string; // Optional property
+      };
+      dominentpol: string;
+      iaqi: {
+        [key: string]: { v: number };
+      };
+      time: {
+        s: string; // Local time string
+        tz: string; // Timezone offset
+        v: number; // UNIX timestamp
+        iso: string; // ISO 8601 format
+      };
+      forecast?: {
+        daily: {
+          [key: string]: {
+            avg: number;
+            day: string;
+            max: number;
+            min: number;
+          }[];
         };
       };
+      debug?: {
+        sync: string;
+      };
+    };
+  } | {
+    status:string;
+    data:string
+  };
+  
 
       const [cityInfo, updateInfo] = useState<AirQualityData | null>(null);
 
     useEffect(()=>{
 
-       queryCity = new URLSearchParams(window.location.search).get('city');
+       const queryCity:string|null = new URLSearchParams(window.location.search).get('city');
        axios.get(`https://api.waqi.info/feed/${queryCity}/?token=${process.env.NEXT_PUBLIC_API_KEY}`)
        .then((res)=>{
             console.log(res.data)
@@ -49,12 +81,26 @@ export default function CalitateAer() {
         </>)
     }
 
+    else if(cityInfo.status == "error" && typeof(cityInfo.data) == "string"){
+
+        return(
+          <>
+          
+         <h1>Eroare! Aceasta statie meteo nu exista!</h1>
+         <Link href="/">Return</Link>
+          </>
+        )
+
+    }
+    else if(typeof(cityInfo.data)!="string")
     return (
         <>
 
         <h1> This is a test!</h1>
         <Link href="/">Return</Link>
-        <h1>Air Quality for {cityInfo.data.city.name}</h1>
+        <h1>Calitate aer pentru: {cityInfo.data.city.name}</h1>
+        <h1>{cityInfo.data.forecast?.daily.pm10[0].avg}</h1>
+        <h1>{cityInfo.data.iaqi['pm10']?.v}</h1>
         </>
         
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import {useRouter} from 'next/navigation'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import  styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import ROFlag from '../styles/icons/ro-flag.png'
 
 export default function Home(){
 
@@ -10,6 +13,8 @@ export default function Home(){
 
     const [city, selectCity] = useState<string>("")
 
+    const [datetime, setDateTime] = useState<string>(Date().slice(0, 24))
+    
     const changeCity = (event: React.ChangeEvent<HTMLSelectElement>) => {
         selectCity(event.target.value)
     }
@@ -20,19 +25,80 @@ export default function Home(){
         }
     }
 
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDateTime(Date().slice(0, 24));
+        }, 1000);
+        return () => clearInterval(intervalId)
+    }, [])
+
     return(
         <>
-        
-        <h1> Alege un oras:</h1>
-        
-        <select name="city" defaultValue="default" onChange={changeCity}>
-          <option value="default" disabled></option>
-          <option value="bucuresti">Bucuresti</option>
-          <option value="vaslui">Vaslui</option>
-        </select>
-        
-        <button onClick={handleSubmit}>OK</button>
+        <div className = {styles.header_bar}>
+        <Image className = {styles.ro_flag} src = {ROFlag} alt="RO-flag" width={30} height={20}></Image>
+        <span className = {styles.app_title}> AerPentruToți - calitatea aerului în România zi de zi</span>
+        <span className = {styles.date_time}> {datetime}</span>
+        </div>
 
+
+
+        <div className = {styles.page_container} >
+
+        <h1 className = {styles.select_title}> Alege un oraș</h1>
+        <div className = {styles.city_sel_wrap}>
+
+        <select className={styles.city_selector} name="city" defaultValue="default" onChange={changeCity}>
+            <option value = "default" disabled></option>
+            <option value="Bucuresti">Bucuresti</option>
+            <option value="alba-iulia">Alba Iulia</option>
+            <option value="arad">Arad</option>
+            <option value="bacau">Bacau</option>
+            <option value="baia-mare">Baia Mare</option>
+            <option value="botosani">Botosani</option>
+            <option value="brasov">Brasov</option>
+            <option value="sinaia">Sinaia</option>
+            <option value="buzau">Buzau</option>
+            <option value="cluj">Cluj</option>
+            <option value="constanta">Constanta</option>
+            <option value="craiova">Craiova</option>
+            <option value="deva">Deva</option>
+            <option value="drobeta-turnu-severin">Drobeta-Turnu Severin</option>
+            <option value="focsani">Focsani</option>
+            <option value="galati">Galati</option>
+            <option value="iasi">Iasi</option>
+            <option value="lugoj">Lugoj</option>
+            <option value="mangalia">Mangalia</option>
+            <option value="medgidia">Medgidia</option>
+            <option value="oradea">Oradea</option>
+            <option value="piatra-neamt">Piatra Neamt</option>
+            <option value="pitesti">Pitesti</option>
+            <option value="ploiesti">Ploiesti</option>
+            <option value="ramnicu-valcea">Ramnicu Valcea</option>
+            <option value="resita">Resita</option>
+            <option value="satu-mare">Satu Mare</option>
+            <option value="sibiu">Sibiu</option>
+            <option value="sfantu-gheorghe">Sfantu Gheorghe</option>
+            <option value="slatina">Slatina</option>
+            <option value="slobozia">Slobozia</option>
+            <option value="suceava">Suceava</option>
+            <option value="targoviste">Targoviste</option>
+            <option value="targu-jiu">Targu Jiu</option>
+            <option value="targu-mures">Targu Mures</option>
+            <option value="timisoara">Timisoara</option>
+            <option value="tulcea">Tulcea</option>
+            <option value="vaslui">Vaslui</option>
+            <option value="zalau">Zalau</option>
+
+
+        </select>
+
+        </div>
+        
+        {city? <button className = {styles.ok_button} onClick={handleSubmit}>Confirmă</button> : <p></p>}
+        
+
+        </div>
         </>
     );
 
